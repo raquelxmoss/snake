ddca78d801223f1b69b45b27a38d5c7c070c8934
Raquel Moss <raquelxmoss@gmail.com>
2017-10-14 17:31:41 +1300

    Render an empty canvas

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,36 @@
 import { run } from '@cycle/run';
 import xs, { Stream } from 'xstream';
 import { VNode, DOMSource, makeDOMDriver, div } from '@cycle/dom';
+const { makeCanvasDriver, rect } = require('cycle-canvas');
 
 type Sources = {
-  DOM : DOMSource;
+  // DOM : DOMSource;
 }
 
 type Sinks = {
-  DOM : Stream<VNode>;
+  // DOM : Stream<VNode>;
+  Canvas : Stream<any>;
 }
 
 export function main (sources : Sources) : Sinks {
   return {
-    DOM: xs.of(div('hello world'))
+    // DOM: xs.of(div('hello world')),
+    Canvas: xs.of(
+      rect({
+        x: 1,
+        y: 1,
+        width: 400,
+        height: 600,
+        draw: [{stroke: '#000'}],
+        children: []
+      })
+    )
   }
 }
 
 const drivers = {
-  DOM: makeDOMDriver('#app')
+  // DOM: makeDOMDriver('#app'),
+  Canvas: makeCanvasDriver('#snake', {width: 605, height: 905 })
 }
 
 run(main, drivers)
