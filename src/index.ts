5f6bcabd2cc38f6eda9d981805ca9dbc5d8d8dc3
Raquel Moss <raquelxmoss@gmail.com>
2017-10-14 17:56:38 +1300

    Render an egg

--- src/index.ts
+++ src/index.ts
@@ -12,17 +12,61 @@ type Sinks = {
   Canvas : Stream<any>;
 }
 
+type Vector = {
+  x : number;
+  y : number;
+}
+// given a blank canvas
+// spawn a single egg within the bounds of the canvas
+
+type Egg = {
+  position : Vector;
+  height : number;
+  width : number;
+  color? : string;
+}
+
+const CONTAINER_WIDTH = 400;
+const CONTAINER_HEIGHT = 600;
+
+function rand(min : number = 0, max : number = 1000) : number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function renderEgg(egg : Egg) : any {
+  return rect({
+    x: egg.position.x,
+    y: egg.position.y,
+    width: egg.width,
+    height: egg.height,
+    draw: [{fill: egg.color || 'red'}]
+  })
+}
+
 export function main (sources : Sources) : Sinks {
+  const egg : Egg = {
+    position: {
+      x: rand(0, CONTAINER_WIDTH),
+      y: rand(0, CONTAINER_HEIGHT)
+    },
+    height: 10,
+    width: 10
+  }
+
+  const eggs : Egg[] = [egg];
+
   return {
     // DOM: xs.of(div('hello world')),
     Canvas: xs.of(
       rect({
         x: 1,
         y: 1,
-        width: 400,
-        height: 600,
+        width: CONTAINER_WIDTH,
+        height: CONTAINER_HEIGHT,
         draw: [{stroke: '#000'}],
-        children: []
+        children: eggs.map(renderEgg)
       })
     )
   }
